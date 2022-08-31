7b9bb9ef6ab6530e92eb9b258234ac768f89f707
Dré Van den Hooff <dre.vandenhooff@student.hogent.be>
2022-08-31 16:11:33 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./theme";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Login />} path="/login" />
+          <Route element={<Register />} path="/login" />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
   );
 }
 
