e64cac56b113d56faa3ab43c1818a24ff76403ce
Dre Van den Hooff <dre.vandenhooff@student.hogent.be>
2022-09-04 16:19:10 +0200

    start authprovider

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Login from "./pages/login";
 import Register from "./pages/register";
 import Leaderboard from "./pages/leaderboard";
 import Profile from "./pages/profile";
+import AuthProvider from "./auth";
 
 function App() {
   const client = new ApolloClient({ cache: new InMemoryCache(), uri: "http://localhost:4000/graphql" });
@@ -13,15 +14,17 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <ChakraProvider theme={theme}>
-        <Router>
-          <Routes>
-            <Route element={<Login />} path="/login" />
-            <Route element={<Register />} path="/register" />
-            <Route element={<Leaderboard />} path="/" />
-            <Route element={<Profile />} path="/profile" />
-            <Route element={<Leaderboard />} path="/leaderboard" />
-          </Routes>
-        </Router>
+        <AuthProvider>
+          <Router>
+            <Routes>
+              <Route element={<Login />} path="/login" />
+              <Route element={<Register />} path="/register" />
+              <Route element={<Leaderboard />} path="/" />
+              <Route element={<Profile />} path="/profile" />
+              <Route element={<Leaderboard />} path="/leaderboard" />
+            </Routes>
+          </Router>
+        </AuthProvider>
       </ChakraProvider>
     </ApolloProvider>
   );
