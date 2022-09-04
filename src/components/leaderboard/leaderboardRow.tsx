9f1f3c32534faa807107022cf41a9fe6e7f8a104
Dre Van den Hooff <dre.vandenhooff@student.hogent.be>
2022-09-04 11:21:53 +0200

    WIP leaderboardrow

--- src/components/leaderboard/leaderboardRow.tsx
+++ src/components/leaderboard/leaderboardRow.tsx
@@ -1,12 +1,23 @@
 import { LeaderboardRowProps } from "../../interfaces";
-import { Flex, Text, Center } from "@chakra-ui/react";
+import { Flex, Text, Center, Avatar } from "@chakra-ui/react";
 
-const LeaderboardRow = ({ id, username, stats }: LeaderboardRowProps) => {
+const LeaderboardRow = ({ id, username, amount, rank }: LeaderboardRowProps) => {
   return (
     <Center p="0.5rem">
-      <Flex justifyContent="space-between" maxW="60%" w="100%" bgColor="teal.200" p="0.5rem" borderRadius="1rem">
+      <Flex
+        justifyContent="space-between"
+        alignItems="center"
+        maxW="90%"
+        w="100%"
+        bgColor="teal.200"
+        p="0.5rem"
+        borderRadius="1rem">
+        <Flex alignItems="center">
+          <Text mr="20%">{rank + 1}</Text>
+          <Avatar name={username} />
+        </Flex>
         <Text>{username}</Text>
-        <Text>{stats.worstenbroodjes}</Text>
+        <Text>{amount}</Text>
       </Flex>
     </Center>
   );
