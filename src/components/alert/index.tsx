893975b180f22924a11e35ae2ed709ba0e38d6db
Dré Van den Hooff <dre.vandenhooff@student.hogent.be>
2022-09-05 15:48:54 +0200

    add alert

---
+++ src/components/alert/index.tsx
@@ -0,0 +1,24 @@
+import { Flex, Alert, AlertIcon, AlertTitle, AlertDescription, Button, type AlertProps } from "@chakra-ui/react";
+import { Link as RouteLink } from "react-router-dom";
+import { FiLogIn } from "react-icons/fi";
+import { MdOutlineAccountCircle } from "react-icons/md";
+
+const NotLoggedInAlert = ({ mt, mb, w }: AlertProps) => {
+  return (
+    <Alert status="error" flexDirection="column" borderRadius="1rem" mt={mt} mb={mb} w={w}>
+      <AlertIcon boxSize="25px" mb="1rem" />
+      <AlertTitle>Je bent niet ingelogd</AlertTitle>
+      <AlertDescription>Log in of maak een account om verder te gaan.</AlertDescription>
+      <Flex justifyContent="space-evenly" w="100%" my="2rem">
+        <Button as={RouteLink} to="/login" bgColor="teal" leftIcon={<FiLogIn />}>
+          Inloggen
+        </Button>
+        <Button as={RouteLink} to="/register" bgColor="teal" leftIcon={<MdOutlineAccountCircle />}>
+          Registreren
+        </Button>
+      </Flex>
+    </Alert>
+  );
+};
+
+export default NotLoggedInAlert;
